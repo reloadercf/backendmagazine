3455284d0d499809a1e1f5b0cf5bf16e7a6a6866
MarioAV14 <mvaldez7497@gmail.com>
2019-08-23 15:13:51 -0500

    Acondicionamiento

--- youmagazinebackend/settings.py
+++ youmagazinebackend/settings.py
@@ -37,12 +37,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'articulos',
+    'cliente',
+    'revista',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',  
+    'import_export',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
