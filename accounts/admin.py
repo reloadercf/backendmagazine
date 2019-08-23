210c06f002707a5aabf01807dcafd4a3f668ce10
reloadercf <elcfm1@gmail.com>
2019-08-23 14:13:59 -0500

    todo magazine

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Articulo
+from .models import Perfil
 # Register your models here.
 
-admin.site.register(Articulo)
+admin.site.register(Perfil)
