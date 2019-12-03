03233d7210e3202148a9121f17ed00aebff7d012
MarioAV14 <mvaldez7497@gmail.com>
2019-12-03 13:54:45 -0600

    insercion de variable de entorno

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from django.db.models import Q
+from youmagazinebackend.settings import key
 
 #vista para visualizacion de permisos
 class PermissionViewSet(viewsets.ReadOnlyModelViewSet):
@@ -68,7 +69,18 @@ class MyUser(APIView):
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate('serviceAccountKey.json')
+cred = credentials.Certificate({
+  "type": "service_account",
+  "project_id": "youmagazine-109a9",
+  "private_key_id":key,
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDcU0kW7AA2xwi7\nrVj3mMCXmD5g1LGTmG2IkhNWEUMQBZj3jqRfoNO1Y6aGk2ET2DKdrJNVPA73dOSG\nuBcY8fN+8DwO4LQMmi/Yx73G2nX4bL9mdPhVO37hQWnoHk2aPpXoFaCQJHOJfL+a\ned4e1mG9Eqs+3K8mLu8MpO9uP3RML+4a6j2PcxN5h4XSfIHPpx2RyrHCqGbEIROY\n4bJPxUDUy0eWto2VW5hpMvCXK1U5lhdXsbwT5Mu4xOQy21Jl58wJ526s0RSsHBKd\nJfFaizlh8MOxXcoQodRVYMYGrs+JxbM5QCzSReNjAG4TiCFSDCr29BvDaK+dDq0O\n9leHl85RAgMBAAECggEAIkP/s4VruRk2Vf3tPsCuf0UpumSJ45MKwfk58aJIpEbC\nlgnEray1EFpZrUdhizUOFjCfT1vCViEDY6Jg0Tvb60uQg53V0rnuJBBYhwM14reD\nAvvBHXxdzMmgH34LAqimJsyqRpCsuV7B69P1RCWCWOX85Q9sXAtjmTtTk3lh7HEj\n/TZ6d9Ro1Xr6zlVpZrqNdSybip6xSis7rqL5aVm9CNfmZ6bxIxysyMw58rqrPGhZ\nlPjfoCmwD7rYAbfcpJx8V1vxBSq8RH1Mc4ILZsTrlF0xArCkxX0n+/JIDt/r9vbn\nm4na0SQAzUDcfNzdSWTJ//0z7u+jHWQodXuv4MYcuQKBgQDwzAkjM6Gm+Ru+KK+h\n1Y9oUQxsr2nbnTeoRAqALiPh+3XuI5uV4y3wdF88CCXtk3DWXIgDrv0O5wgfisvD\nLj/k9yFkkIs0dg+7NIwRYjZt8etnrfiwOYmLw+wl9WAoINI9guP/EM1C1GiIW9JN\nbzY8tI/ej2+bB6K+gZgUNWERBQKBgQDqPF6HvvGExFc6OW+mKvIEGbkdkuFp8Fuw\nyCWFO9Xt/5mcHr4q2Cv50wUXOuC8U8+7fchQ5pLp2vHopZpICbO1ZZNbf20vT1bA\nJjffXzaCCj32brn4eDgU5qTi8u3owN/noTNK7b5aH7Zh+dVLfzjBF8a8Q5GMg6hK\nYx5bhZo53QKBgQDqiFB+ENPqJcd3t3kZT0CWghpv4YphdQe4xEzV/BrHCH0crWHk\n45jjEvWPKwCGHY1RvF5d+BpSYYPv9Ofx+yIQiQPlR0POnQ9FbzDd3aLWA0MdgL0w\nJ1po3zMq5Yv6ib08XDhlPqAt72TBK23yLdfN2LAoPdyeSxmgI+lUBYm9YQKBgGw1\nn2YOq/ytx646Qn39cPi7+WzUIhaS14j/rXWzgs0EO66H9UpNu2MaPUvSA2h9P4Za\nQWQ+YLhwUTks5+1HriGq3zxxzbsBaA40mbMzhTbmDGIAHSHQR/g/5QSDPa09DTMw\noUWIN1vjMppolETgbROnLERk4qSLXf+fcrLNMcZxAoGBAJQttB7P/iGPWlFXyB5l\nqrLwJU1qC2nXLvBF0KdTXrImghR6LeaYLZdYrMGGQcL5fvsqSX3n5YVmhON4bs17\niJ+YlUyJDvobWc4UvAviaJCJmsfB/+1RC4s+V0OKhIIqQluPyJb/xVXnHdbhQR4e\nX+tdwr9jMYaAbRoToZKnLnoS\n-----END PRIVATE KEY-----\n",
+  "client_email": "youmagazine-109a9@appspot.gserviceaccount.com",
+  "client_id": "107047606938047887476",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/youmagazine-109a9%40appspot.gserviceaccount.com"
+})
 default_app = firebase_admin.initialize_app(cred)
 
 class CustomAuthToken(ObtainAuthToken):
--- youmagazinebackend/settings.py
+++ youmagazinebackend/settings.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import datetime
 import os
+import environ
+
+env = environ.Env(
+    KEY=str,    
+)
+
+key = env('KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
